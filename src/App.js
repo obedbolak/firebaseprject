2614b2a203fe839092b5d2bfde889efa67c908aa
obedbolak <jamesnormankate@gmail.com>
2024-03-30 07:59:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React now and fast
         </a>
       </header>
     </div>
