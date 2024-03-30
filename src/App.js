da46ab82dffd13e8408ca6898d24b706fc7af414
unknown <unicm@gmail.com>
2024-03-30 13:29:45 -0400

    first cmmit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now and fast
+          Learn React now and fast realtime
         </a>
       </header>
     </div>
